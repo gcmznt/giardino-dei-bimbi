3b2725c1b73f3dba87de407a06b988388a98214f
Giacomo Zinetti <giacomo.zinetti@gmail.com>
2019-04-22 13:19:26 +0200

    fix news

--- docroot/assets/themes/giardino-dei-bimbi/loop-news.php
+++ docroot/assets/themes/giardino-dei-bimbi/loop-news.php
@@ -1,26 +1,30 @@
-<section class="news-wrapper">
-  <h1>Bacheca avvisi</h1>
-  <?php
-    $today = date("Y-m-d");
-    
-    $query = new WP_Query(array(
-      "post_type" => 'post',
-      "posts_per_page" => 3,
-      "post_status" => "publish",
-    ));
-    
-    if ($query->have_posts()) {
-      while ( $query->have_posts() ) {
-        $query->the_post();
-        ?>
-          <article class="news">
-            <h2><?php the_title(); ?></h2>
-            <h4><?php echo get_the_date(); ?></h4>
-            <?php the_content(); ?>
-          </article>
-        <?php
-      }
-      wp_reset_postdata();
+<?php
+  $today = date("Y-m-d");
+  
+  $query = new WP_Query(array(
+    "post_type" => 'post',
+    "posts_per_page" => 3,
+    "post_status" => "publish",
+  ));
+  
+  if ($query->have_posts()) {
+?>
+    <section class="news-wrapper">
+      <h1>Bacheca avvisi</h1>
+<?php
+    while ( $query->have_posts() ) {
+      $query->the_post();
+      ?>
+        <article class="news">
+          <h2><?php the_title(); ?></h2>
+          <h4><?php echo get_the_date(); ?></h4>
+          <?php the_content(); ?>
+        </article>
+      <?php
     }
-  ?>
-</sectio>
+    wp_reset_postdata();
+?>
+    </section>
+<?php
+  }
+?>
