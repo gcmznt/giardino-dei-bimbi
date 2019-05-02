315b5bf9a1765416f54b2a7a7863d69cb1911d61
Giacomo Zinetti <giacomo.zinetti@gmail.com>
2019-05-02 10:31:58 +0200

    show tree only on the home

--- docroot/assets/themes/giardino-dei-bimbi/header.php
+++ docroot/assets/themes/giardino-dei-bimbi/header.php
@@ -16,8 +16,10 @@
       <h1>Il Giardino dei Bimbi</h1>
       <a href="#" class="burger" onclick="this.classList.toggle('is-open'); return false;"></a>
       <?php main_menu(); ?>
-      <img src="<?php echo get_bloginfo('template_directory'); ?>/images/tree.png" class="tree" alt="" />
-      <div class="clouds">
-        <svg width="1335" height="220" xmlns="http://www.w3.org/2000/svg"><path d="M.918 227.344h1334.117c-21.917-47.12-57.278-58.9-106.086-35.34-61.738-65.426-131.933-55.867-155.289-47.625C1015.344 65.523 834.21-24.684 686.125 130c-64.191-36.992-101.094-21.816-136.68-13.676-33.777-145.582-267.996-168.758-305.46 20.153-48.372-46.61-203.266-51-243.067 90.867z" fill="currentColor" fillRule="evenodd"/></svg>
-      </div>
+      <?php if (is_home()) { ?>
+        <img src="<?php echo get_bloginfo('template_directory'); ?>/images/tree.png" class="tree" alt="" />
+        <div class="clouds">
+          <svg width="1335" height="220" xmlns="http://www.w3.org/2000/svg"><path d="M.918 227.344h1334.117c-21.917-47.12-57.278-58.9-106.086-35.34-61.738-65.426-131.933-55.867-155.289-47.625C1015.344 65.523 834.21-24.684 686.125 130c-64.191-36.992-101.094-21.816-136.68-13.676-33.777-145.582-267.996-168.758-305.46 20.153-48.372-46.61-203.266-51-243.067 90.867z" fill="currentColor" fillRule="evenodd"/></svg>
+        </div>
+      <?php } ?>
     </section>
