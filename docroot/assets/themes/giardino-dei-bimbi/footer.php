ba72e6913e9ddeb41861205a8621358b9b2c70a9
Giacomo Zinetti <giacomo.zinetti@gmail.com>
2019-05-14 15:00:07 +0200

    tidio

--- docroot/assets/themes/giardino-dei-bimbi/footer.php
+++ docroot/assets/themes/giardino-dei-bimbi/footer.php
@@ -1,3 +1,12 @@
   </main>
+  <?php if (is_user_logged_in()) { ?>
+    <script src="//code.tidio.co/mxiqrkti4zhkbk8o9z4nvcgotowwpd6s.js"></script>
+    <script>
+      document.tidioIdentify = {
+        name: '<?php echo wp_get_current_user()->display_name; ?>',
+        email: '<?php echo wp_get_current_user()->user_email; ?>',
+      };
+    </script>
+  <?php } ?>
 </body>
 </html>
